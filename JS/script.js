4b60b6610dfca803cd566ef10939ecc367202519
Moses Mwaniki <mwanikimose@gmail.com>
2024-02-03 21:57:21 +0300

    nav links

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
 const stickyNav = document.getElementById('navScroll');
-const mplistnav = document.getElementById('trials');
-const mplist = document.getElementById('trials2');
-const mplisthme = document.getElementById('hme');
+const mplistnav = document.getElementById('heroNav');
+const mplist = document.getElementById('heroText');
+const mplisthme = document.getElementById('home');
 const mplistabt = document.getElementById('abtme');
 
 const initialHeight = stickyNav.offsetHeight;
@@ -9,15 +9,16 @@ const initialHeight = stickyNav.offsetHeight;
 window.addEventListener('scroll', () => {
   if (window.scrollY > initialHeight) {
     stickyNav.classList.add('shrink');
-    mplist.classList.add('trial');
+    mplist.classList.add('heroExpand');
     mplisthme.style.display = 'inline';
     mplistnav.style.width = '0vw';
     mplistabt.style.display = 'none';
   } else {
     stickyNav.classList.remove('shrink');
-    mplist.classList.remove('trial');
-    mplistabt.style.display = 'inline';
+    mplist.classList.remove('heroExpand');
+    // mplistabt.style.display = 'inline';
     mplisthme.style.display = 'none';
     mplistnav.style.width = '45vw'; // Add class for animation
   }
 });
+// ====================
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,23 +1,51 @@
 const stickyNav = document.getElementById('navScroll');
 // const mplistnav = document.getElementById('triall');
 const mplist = document.getElementById('trials2');
-const mplisthme = document.getElementById('homes');
-const mplistabt = document.getElementById('abtme');
+const wplistTop = document.getElementById('top');
+// const mplistabt = document.getElementById('abtme');
 
 const initialHeight = stickyNav.offsetHeight;
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > initialHeight) {
-    stickyNav.classList.add('shrink');
+    stickyNav.classList.add('navShrink');
     mplist.classList.add('trials32');
-    mplisthme.style.display = 'inline';
-    mplistnav.style.width = '0vw';
-    mplistabt.style.display = 'none';
+    wplistTop.style.display = 'inline';
+    // mplistnav.style.width = '0vw';
+    // mplistabt.style.display = 'none';
   } else {
-    stickyNav.classList.remove('shrink');
+    stickyNav.classList.remove('navShrink');
     mplist.classList.remove('trials32');
-    mplistabt.style.display = 'inline';
-    mplisthme.style.display = 'none';
-    mplistnav.style.width = '45vw'; // Add class for animation
+    // mplistabt.style.display = 'inline';
+    wplistTop.style.display = 'none';
+    // mplistnav.style.width = '45vw'; // Add class for animation
   }
+});
+// ===================
+
+const navlinksEls = document.querySelectorAll('.wpList a');
+const sectionsEls = document.querySelectorAll('.section');
+
+let currentSection = 'home';
+
+window.addEventListener('scroll', () => {
+  // let activeSectionId; // Store the ID of the active section
+
+  sectionsEls.forEach(sectionsEl => {
+    if (window.scrollY >= sectionsEl.offsetTop - 200) {
+      currentSection = sectionsEl.id; // Access the ID of the current section
+      // Exit the loop as soon as the active section is found
+    }
+  });
+
+  // currentSection = activeSectionId; // Assign the found ID to currentSection
+  console.log(currentSection);
+
+  navlinksEls.forEach(navlinksEl => {
+    if (navlinksEl.href && navlinksEl.href.includes(currentSection)) {
+      document.querySelector('.active').classList.remove('active');
+      navlinksEl.classList.add('active');
+    }
+    console.log(navlinksEl);
+  });
 });
